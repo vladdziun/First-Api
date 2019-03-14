124e3664b1f55416e78c8b3ec6a452a08b0dc756
prescottbreeden <prescott.breeden@gmail.com>
2019-03-13 18:27:32 -0700

    Prescott Code Review
    Prescott's one on one time with Vlad playing with javascript and vim
    cause vim is cool

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 8000;
+const app = express();
+
 app.use(bodyParser.json());
-var path = require('path');
-app.use(express.static(path.join(__dirname, './static')));
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-
+app.listen(port, () => console.log(`listening on port ${port}`));
 require('./server/routes.js')(app);
-
-app.listen(8000, function () {
-    console.log("listening on port 8000");
-}) 
