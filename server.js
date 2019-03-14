d3220701eb077e21763a4bc7b0d16ea8a09689cb
Vlad Dziun <v.dziun@gmail.com>
2019-03-14 14:12:06 -0700

    add angular

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const port = 8000;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static( __dirname + '/public/dist/public' ));
 app.listen(port, () => console.log(`listening on port ${port}`));
 require('./server/routes.js')(app);
