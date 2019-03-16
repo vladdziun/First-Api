903c2a756bb0833cf5a371e92e570693bbfb5daa
Vlad Dziun <v.dziun@gmail.com>
2019-03-16 13:47:06 -0700

    display data in html component

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,12 +6,21 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  persons = [];
 
-  constructor(private _httpService: HttpService)
-  {
-
+  constructor(private _httpService: HttpService) {
   }
-  
+  ngOnInit() {
+    this.getPersonsFromService()
+  }
+  getPersonsFromService() {
+    let observable = this._httpService.getPersons()
+    observable.subscribe(data => {
+      console.log("got our data", data);
+      this.persons.push(data);
+      console.log(this.persons);
+    })
+  }
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -4,11 +4,18 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class HttpService {
   constructor(private _http: HttpClient) {
-    this.getPersons();
+    // this.getPersons();
+    // this.getPokemon();
   }
 
   getPersons() {
-    let tempObservable = this._http.get('/api/persons');
-    tempObservable.subscribe(data => console.log("Got our persons!", data));
+    // let tempObservable = this._http.get('/api/persons');
+    // tempObservable.subscribe(data => console.log("Got our persons!", data));
+    return this._http.get('/api/persons')
+  }
+  getPokemon()
+  {
+    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon');
+    bulbasaur.subscribe(data => console.log("Got our pokemon!", data));
   }
 }
