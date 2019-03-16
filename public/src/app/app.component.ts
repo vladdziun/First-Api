868e393e45207f2d551c26cfbef3edea9602d05c
Vlad Dziun <v.dziun@gmail.com>
2019-03-16 15:10:05 -0700

    add click events

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -9,18 +9,32 @@ import { HttpService } from './http.service';
 export class AppComponent implements OnInit {
   title = 'app';
   persons = [];
+  id = "";
 
   constructor(private _httpService: HttpService) {
   }
   ngOnInit() {
-    this.getPersonsFromService()
+    // this.getPersonsFromService()
   }
   getPersonsFromService() {
+    console.log("Here!")
     let observable = this._httpService.getPersons()
     observable.subscribe(data => {
       console.log("got our data", data);
       this.persons.push(data);
       console.log(this.persons);
     })
+  }
+  onButtonClick(name: String): void{
+    console.log(`Click working with param ${name}`);
+
+    let observable = this._httpService.postToServer({name});
+    observable.subscribe(data => console.log("Got data", data));
+  }
+  showInfo(id: string): void{
+    console.log(`showing info ${id}`);
+    let observable = this._httpService.getOnePerson(id);
+    observable.subscribe(data => console.log(data));
+    this.id = id;
   }
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -11,11 +11,18 @@ export class HttpService {
   getPersons() {
     // let tempObservable = this._http.get('/api/persons');
     // tempObservable.subscribe(data => console.log("Got our persons!", data));
-    return this._http.get('/api/persons')
+    return this._http.get('/api/persons');
+  }
+  getOnePerson(id){
+    return this._http.get(`/api/persons/${id}`);
   }
   getPokemon()
   {
     let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon');
     bulbasaur.subscribe(data => console.log("Got our pokemon!", data));
+  }
+  postToServer(name)
+  {
+    return this._http.post('/api/persons', name);
   }
 }
